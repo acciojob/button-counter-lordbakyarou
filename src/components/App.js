b0f508634579054c458940884022619a29e9b1dc
Mayur Hanwate <frostblizard111@gmail.com>
2023-12-07 17:59:28 +0530

    commit this

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Button Clicked {num} times</p>
+      <button onClick={setNum(num + 1)}>Click Me</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
